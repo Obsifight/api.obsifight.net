fba37ab20a4c7715a284a412208b7b726bac5dd6
Valentin <contact@eywek.fr>
2016-11-13 20:14:44 +0100

    Use username to find user

--- app/models/User.js
+++ app/models/User.js
@@ -2,43 +2,58 @@ var async = require('async')
 
 module.exports = {
 
-  getIds: function (id, next) {
+  getIds: function (username, next) {
     var result = {
       web: 0,
       logblock: 0,
       auth: 0
     }
-    if (id === undefined) return result
+    if (username === undefined) return result
 
-    async.parallel([
-      // web
-      function (callback) {
-        db.get('web_v5').query("SELECT id FROM users WHERE `id` = '" + id + "' OR `pseudo` = '" + id + "' LIMIT 1", function (err, rows, fields) {
-          if (err) return callback(err)
-          callback(undefined, rows)
-        })
-      },
-      // auth
-      function (callback) {
-        db.get('auth').query("SELECT * FROM `joueurs` WHERE `user_id` = '" + id + "' OR `user_pseudo` = '" + id + "'", function (err, rows, fields) {
-          if (err) return callback(err)
-          callback(undefined, rows)
-        })
-      },
-      // logblock
-      function (callback) {
-        db.get('logblock').query("SELECT * FROM `lb-players` WHERE `playername` = '" + id + "' OR `playerid` = '" + id + "'", function (err, rows, fields) {
-          if (err) return callback(err)
-          callback(undefined, rows)
-        })
-      }
-    ], function (err, results) {
-      if (err) return next(err)
-      if (results[0][0] === undefined) return next(true)
+    // web
+    if (username == parseInt(username)) { // is an id
+      db.get('web_v5').query("SELECT `pseudo` FROM users WHERE `id` = " + username + " LIMIT 1", function (err, rows, fields) {
+        if (err) throw err
+        queries(rows[0].pseudo)
+      })
+    } else {
+      queries(username)
+    }
 
-      result.web = results[0][0].id
-      return next(undefined, result)
-    })
+    function queries (username) {
+      async.parallel([
+        // web
+        function (callback) {
+          db.get('web_v5').query("SELECT `id` FROM users WHERE `pseudo` = '" + username + "' LIMIT 1", function (err, rows, fields) {
+            if (err) return callback(err)
+            callback(undefined, rows)
+          })
+        },
+        // auth
+        function (callback) {
+          db.get('auth').query("SELECT `user_id` FROM `joueurs` WHERE `user_pseudo` = '" + username + "' LIMIT 1", function (err, rows, fields) {
+            if (err) return callback(err)
+            callback(undefined, rows)
+          })
+        },
+        // logblock
+        function (callback) {
+          db.get('logblock').query("SELECT `playerid` FROM `lb-players` WHERE `playername` = '" + username + "' LIMIT 1", function (err, rows, fields) {
+            if (err) return callback(err)
+            callback(undefined, rows)
+          })
+        }
+      ], function (err, results) {
+        if (err) return next(err)
+        if (results[0][0] === undefined) return next(true)
+
+        result.web = results[0][0].id
+        result.auth = results[1][0].user_id
+        result.logblock = results[2][0].playerid
+
+        return next(undefined, result)
+      })
+    }
   }
 
 }
