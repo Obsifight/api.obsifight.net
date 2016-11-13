34159dee7dd4b7cd8944f8227b948e6c2d886f6e
Valentin <contact@eywek.fr>
2016-11-13 20:14:44 +0100

    Use username to find user

--- app/controller/UserController.js
+++ app/controller/UserController.js
@@ -1,11 +1,11 @@
 module.exports = {
 
   get: function (req, res) {
-    if (req.params.id === undefined)
-      return res.status(400).json({status: false, error: 'Missing user id.'})
+    if (req.params.username === undefined)
+      return res.status(400).json({status: false, error: 'Missing user\'s name.'})
 
     // Find user
-    User.getIds(req.params.id, function (err, ids) {
+    User.getIds(req.params.username, function (err, ids) {
       if (err || ids.web === 0) return res.status(404).json({status: false, error: 'User not found.'})
 
       res.json({
