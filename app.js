76f906ce3d359c4a74e406122aea51b04aaab23a
Valentin <contact@eywek.fr>
2016-11-14 18:14:13 +0100

    Add pmx

--- app.js
+++ app.js
@@ -6,6 +6,14 @@ var app = express()
 
 app.use(express.static('public'))
 
+var pmx = require('pmx').init({
+  http: true, // HTTP routes logging (default: true)
+  errors: true, // Exceptions loggin (default: true)
+  custom_probes: true, // Auto expose JS Loop Latency and HTTP req/s as custom metrics
+  network: true, // Network monitoring at the application level
+  ports: true  // Shows which ports your app is listening on (default: false)
+})
+
 // =======================
 // Body & Accept =========
 // =======================
