875acc409a92d74bd16fdf3d95069ad7f17d6b17
Valentin <contact@eywek.fr>
2016-11-23 18:44:18 +0100

    Fix + add /user/:username/vote/can

--- app/config/routes.js
+++ app/config/routes.js
@@ -2,6 +2,8 @@ module.exports = {
 
   'post /authenticate': 'AuthController.generateToken',
 
-  'get /user/:username': {'function': 'UserController.get', protected: true}
+  'get /user/:username': {'function': 'UserController.get', protected: true},
+
+  'get /user/:username/vote/can': 'UserController.canVote'
 
 }
