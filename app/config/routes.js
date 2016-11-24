5a336577f89cab88716a7013a24faac9ffed0066
Valentin <contact@eywek.fr>
2016-11-24 19:46:13 +0100

    Add route /sanction/bans

--- app/config/routes.js
+++ app/config/routes.js
@@ -3,7 +3,8 @@ module.exports = {
   'post /authenticate': 'AuthController.generateToken',
 
   'get /user/:username': {'function': 'UserController.get', protected: true},
+  'get /user/:username/vote/can': 'UserController.canVote',
 
-  'get /user/:username/vote/can': 'UserController.canVote'
+  'get /sanction/bans': {'function': 'SanctionController.getBans', protected: true}
 
 }
