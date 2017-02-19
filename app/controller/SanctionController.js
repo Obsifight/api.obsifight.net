1bc073ee87de7396320fe85650d716bbbc6b37a4
Valentin <contact@eywek.fr>
2017-02-19 19:51:03 +0100

    Add params for /sanction/[mutes|bans] + edit changelog/version

--- app/controller/SanctionController.js
+++ app/controller/SanctionController.js
@@ -116,16 +116,35 @@ module.exports = {
 
   getBans: function (req, res) {
     var limit = 100
+    var count = false
+    var conditions = "1"
     if (req.query !== undefined && req.query.limit !== undefined)
       limit = parseInt(req.query.limit)
+    if (req.query !== undefined && req.query.count !== undefined && req.query.count == 1)
+      count = true
+    if (req.query !== undefined && req.query.date !== undefined)
+      conditions = "`ban_begin` LIKE '" + req.query.date + "'"
 
-    db.get('sanctions').query("SELECT `ban_id` AS `id`, `UUID` AS `uuid`, `ban_ip` AS `banned_ip`, `ban_staff` AS `staff_username`, `ban_reason` AS `reason`, `ban_server` AS `server`, `ban_begin` AS `date`, `ban_end` AS `end_date`, `ban_state` AS `state`, `ban_unbandate` AS `remove_date`, `ban_unbanstaff` AS `remove_staff`, `ban_unbanreason` AS `remove_reason` FROM BAT_ban WHERE 1 ORDER BY `id` DESC LIMIT ?", [limit], function (err, rows, fields) {
+    var fields = "`ban_id` AS `id`, `UUID` AS `uuid`, `ban_ip` AS `banned_ip`, `ban_staff` AS `staff_username`, `ban_reason` AS `reason`, `ban_server` AS `server`, `ban_begin` AS `date`, `ban_end` AS `end_date`, `ban_state` AS `state`, `ban_unbandate` AS `remove_date`, `ban_unbanstaff` AS `remove_staff`, `ban_unbanreason` AS `remove_reason`"
+    if (count)
+      fields = "COUNT(`ban_id`)"
+
+    db.get('sanctions').query("SELECT " + fields + " FROM BAT_ban WHERE " + conditions + " ORDER BY `id` DESC LIMIT ?", [limit], function (err, rows, fields) {
       if (err) {
         console.error(err)
         return res.status(500).json({status: false, error: 'Internal error.'})
       }
       if (rows === undefined || rows[0] === undefined)
         return res.status(404).json({status: false, error: 'No bans found.'})
+
+      if (count) {
+        return res.json({
+          status: true,
+          data: {
+            count: rows[0]['COUNT(`ban_id`)']
+          }
+        })
+      }
 
       // init response var (after formatting)
       var bans = []
@@ -307,16 +326,35 @@ module.exports = {
 
   getMutes: function (req, res) {
     var limit = 100
+    var count = false
+    var conditions = "1"
     if (req.query !== undefined && req.query.limit !== undefined)
       limit = parseInt(req.query.limit)
+    if (req.query !== undefined && req.query.count !== undefined && req.query.count == 1)
+      count = true
+    if (req.query !== undefined && req.query.date !== undefined)
+      conditions = "`mute_begin` LIKE '" + req.query.date + "'"
 
-    db.get('sanctions').query("SELECT `mute_id` AS `id`, `UUID` AS `uuid`, `mute_ip` AS `muted_ip`, `mute_staff` AS `staff_username`, `mute_reason` AS `reason`, `mute_server` AS `server`, `mute_begin` AS `date`, `mute_end` AS `end_date`, `mute_state` AS `state`, `mute_unmutedate` AS `remove_date`, `mute_unmutestaff` AS `remove_staff`, `mute_unmutereason` AS `remove_reason` FROM BAT_mute WHERE 1 ORDER BY `id` DESC LIMIT ?", [limit], function (err, rows, fields) {
+    var fields = "`mute_id` AS `id`, `UUID` AS `uuid`, `mute_ip` AS `muted_ip`, `mute_staff` AS `staff_username`, `mute_reason` AS `reason`, `mute_server` AS `server`, `mute_begin` AS `date`, `mute_end` AS `end_date`, `mute_state` AS `state`, `mute_unmutedate` AS `remove_date`, `mute_unmutestaff` AS `remove_staff`, `mute_unmutereason` AS `remove_reason`"
+    if (count)
+      fields = "COUNT(`mute_id`)"
+
+    db.get('sanctions').query("SELECT " + fields + " FROM BAT_mute WHERE " + conditions + " ORDER BY `id` DESC LIMIT ?", [limit], function (err, rows, fields) {
       if (err) {
         console.error(err)
         return res.status(500).json({status: false, error: 'Internal error.'})
       }
       if (rows === undefined || rows[0] === undefined)
         return res.status(404).json({status: false, error: 'No mutes found.'})
+
+      if (count) {
+        return res.json({
+          status: true,
+          data: {
+            count: rows[0]['COUNT(`mute_id`)']
+          }
+        })
+      }
 
       // init response var (after formatting)
       var mutes = []
