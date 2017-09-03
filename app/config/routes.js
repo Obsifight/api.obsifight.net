72e65b8e025bde208773c2c8101865c92f778e4f
Valentin <contact@eywek.fr>
2017-09-03 17:17:38 +0200

    Add /user/:username/money routes (transfer + get)

--- app/config/routes.js
+++ app/config/routes.js
@@ -1,33 +1,41 @@
 module.exports = {
 
-  'post /authenticate': 'AuthController.generateToken',
+    'post /authenticate': 'AuthController.generateToken',
 
-  'get /user/:username': {'function': 'UserController.get', protected: true},
-  'get /user/from/uuid/:uuid': {'function': 'UserController.getFromUUID', protected: true},
-  'get /user/uuid/from/:username': {'function': 'UserController.getUUIDFromUsername', protected: true},
-  'post /user/authenticate': {'function': 'UserController.authenticate', protected: true},
-  'get /user/:username/sanctions': {'function': 'SanctionController.getUserSanctions', protected: true},
-  'get /user/:username/sanctions/banned': {'function': 'SanctionController.isUserBanned', protected: true},
-  'get /user/:username/sanctions/muted': {'function': 'SanctionController.isUserMuted', protected: true},
-  'get /user/:username/money/timeline': {'function': 'UserController.getMoneyTimeline', protected: true},
-  'get /user/:username/vote/can': 'UserController.canVote',
-  'get /users/staff/:premium?': 'UserController.getStaff',
-  'get /user/:username/stats': {'function': 'UserController.getStats', protected: true},
+    'get /user/:username': {'function': 'UserController.get', protected: true},
+    'get /user/from/uuid/:uuid': {'function': 'UserController.getFromUUID', protected: true},
+    'get /user/uuid/from/:username': {'function': 'UserController.getUUIDFromUsername', protected: true},
+    'post /user/authenticate': {'function': 'UserController.authenticate', protected: true},
+    'get /user/:username/sanctions': {'function': 'SanctionController.getUserSanctions', protected: true},
+    'get /user/:username/sanctions/banned': {'function': 'SanctionController.isUserBanned', protected: true},
+    'get /user/:username/sanctions/muted': {'function': 'SanctionController.isUserMuted', protected: true},
+    'get /user/:username/money/timeline': {'function': 'UserController.getMoneyTimeline', protected: true},
+    'get /user/:username/money': {'function': 'UserController.getMoney', protected: true},
+    'post /user/:username/money/transfer': {'function': 'UserController.transferMoney', protected: true},
+    'get /user/:username/vote/can': 'UserController.canVote',
+    'get /users/staff/:premium?': 'UserController.getStaff',
+    'get /user/:username/stats': {'function': 'UserController.getStats', protected: true},
 
-  'post /user/find': {'function': 'UserController.findUsers', protected: true},
-  'post /user/infos/username': {'function': 'UserController.getUsersInfos', protected: true},
-  'get /user/compare/:username1/:username2': {'function': 'UserController.compareUsers', protected: true},
+    'post /user/find': {'function': 'UserController.findUsers', protected: true},
+    'post /user/infos/username': {'function': 'UserController.getUsersInfos', protected: true},
+    'get /user/compare/:username1/:username2': {'function': 'UserController.compareUsers', protected: true},
 
-  'get /sanction/bans': {'function': 'SanctionController.getBans', protected: true},
-  'get /sanction/bans/:id': {'function': 'SanctionController.getBan', protected: true},
-  'post /sanction/bans': {'function': 'SanctionController.addBan', protected: true},
-  'put /sanction/bans/:id': {'function': 'SanctionController.editBan', protected: true},
-  'get /sanction/mutes': {'function': 'SanctionController.getMutes', protected: true},
-  'get /sanction/mutes/:id': {'function': 'SanctionController.getMute', protected: true},
-  'post /sanction/mutes': {'function': 'SanctionController.addMute', protected: true},
-  'put /sanction/mutes/:id': {'function': 'SanctionController.editMute', protected: true},
+    'get /sanction/bans': {'function': 'SanctionController.getBans', protected: true},
+    'get /sanction/bans/:id': {'function': 'SanctionController.getBan', protected: true},
+    'post /sanction/bans': {'function': 'SanctionController.addBan', protected: true},
+    'put /sanction/bans/:id': {'function': 'SanctionController.editBan', protected: true},
+    'get /sanction/mutes': {'function': 'SanctionController.getMutes', protected: true},
+    'get /sanction/mutes/:id': {'function': 'SanctionController.getMute', protected: true},
+    'post /sanction/mutes': {'function': 'SanctionController.addMute', protected: true},
+    'put /sanction/mutes/:id': {'function': 'SanctionController.editMute', protected: true},
 
-  'get /socials/twitter/authorization/request': {'function': 'Socials/TwitterController.requestAuthorization', protected: false},
-  'get /socials/twitter/authorization/response': {'function': 'Socials/TwitterController.requestResponse', protected: false},
-  'get /socials/twitter/tweets/liked': {'function': 'Socials/TwitterController.getLikedTweets', protected: false}
+    'get /socials/twitter/authorization/request': {
+        'function': 'Socials/TwitterController.requestAuthorization',
+        protected: false
+    },
+    'get /socials/twitter/authorization/response': {
+        'function': 'Socials/TwitterController.requestResponse',
+        protected: false
+    },
+    'get /socials/twitter/tweets/liked': {'function': 'Socials/TwitterController.getLikedTweets', protected: false}
 }
